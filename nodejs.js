c4134b55e207332fe7f1e730f2b4d4b3bdc42c83
MANOJ NERKAR <79189614+manojnerkar@users.noreply.github.com>
2022-06-07 15:07:17 +0530

    first commit

---
+++ nodejs.js
@@ -0,0 +1,32 @@
+// const app = require('./index')
+
+// console.log(app.x+app.y+app.z)
+
+// var arr = [1,2,3,4,5,2]
+
+// var result = arr.filter((itmes)=>{
+//     return itmes<2;
+// })
+
+// console.log(result)
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync('created2.htmlt')
+
+// console.log()
+
+
+// const http = require('http')
+
+// http.createServer((req,res)=>{
+//     res.write('./he')
+//     res.end();
+// }).listen(8080)
+
+const colors = require('colors')
+const chalk = require('chalk')
+
+console.log('Manoj',chalk)
+console.log('Manoj'.cyan)
